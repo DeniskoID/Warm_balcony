d82658006bfcf0f3e9f81148f4a6aef8adcc4ce1
Denis <80954428+DeniskoID@users.noreply.github.com>
2023-01-06 14:59:37 +0300

    adds tabs and modals scripts

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -4,14 +4,16 @@ const modals = () => {
     const modal = document.querySelector(modalSelector);
     const close = document.querySelector(closeSelector);
 
-    trigger.addEventListener('click', (e) => {
-      if (e.target) {
-        e.preventDefault();
-      }
+    trigger.forEach((item) => {
+      item.addEventListener('click', (e) => {
+        if (e.target) {
+          e.preventDefault();
+        }
 
-      modal.style.display = 'block';
-      document.body.style.overflow = 'hidden';
-      // document.body.classList.add('modal-open');
+        modal.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+        // document.body.classList.add('modal-open');
+      });
     });
 
     close.addEventListener('click', () => {
@@ -28,12 +30,20 @@ const modals = () => {
       }
     });
   }
+  // Показать модальное окно через какое-то время
 
-  const callEngineerBtn = document.querySelector('.popup_engineer_btn');
-  const modalEngineer = document.querySelector('.popup_engineer');
-  const modalEngineerClose = document.querySelector('.popup_engineer .popup_close');
+  function showModalByTime(selector, time) {
+    setTimeout(() => {
+      document.querySelector(selector).style.display = 'block';
+      document.body.style.overflow = 'hidden';
+    }, time);
+  }
 
-  bindModal(callEngineerBtn, modalEngineer, modalEngineerClose);
+  bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+  bindModal('.phone_link', '.popup', '.popup .popup_close');
+
+  // Показать модальное окно через 60сек
+  // showModalByTime('.popup', 60000);
 };
 
 export default modals;
